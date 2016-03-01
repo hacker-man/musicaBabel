1bbc95a5d9d17b0bc4521327c881418f9f4624ca
Lautaro German <lautaro.zuniga@babel.es>
2016-03-01 14:32:20 +0100

    Se ha añadido la funcionalidad al botón de editar canción.

--- js/editarCancion.js
+++ js/editarCancion.js
@@ -1,17 +1,61 @@
- // $(document).ready(function() {
- //     //Cuando se haga click en el boton eliminar de la información
- //     //de la canción incrustada en nuestro código html se desincrustara
- //     //y se eliminara del servidor.
- //     $("#musicaList").on("click", "#buttonEditar", function() {
- //         console.log("Editar canción");
- //         var id = $(this).data("musicaid");
- //         var self = this; //Se refiere al button
- //         $(self).parent().append("<form novalidate> <input type= 'text' id= 'artista' placeholder= 'Artista' name='artista' required> <input type= 'text' id=titulo placeholder= 'Titulo' name='titulo' required> <input type='text' id='url' placeholder='URL' name='titulo' required> <button type='submit'>Guardar</button><button id = 'salir'>Salir</button></form>");
- //         // $.ajax({
- //         //     url: "/api/musica/" + id,
- //         //     method: "get",
- //     });
- //    // $(".main.content").on("click", "#salir", function() {
- //    //     $("form").remove();
- //    // });
- // });
+  $(document).ready(function () {
+      //Cuando se haga click en el boton eliminar de la información
+      //de la canción incrustada en nuestro código html se desincrustara
+      //y se eliminara del servidor.
+      $("#musicaList").on("click", "#buttonEditar", function () {
+          var self = this;
+          $(self).attr("disabled", true);
+          console.log("Editar canción");
+          var id = $(this).data("musicaid");
+          var self = this; //Se refiere al button
+          $(self).parent().append("<form novalidate> <input type= 'text' id= 'artista' class = 'artistaClase' placeholder= 'Artista' name='artista' required> <input type= 'text' id=titulo class = 'tituloClase' placeholder= 'Titulo' name='titulo' required> <input type='text' id='url' class = 'urlClase' placeholder='URL' name='titulo' required> <button type='submit'>Guardar</button><button type = 'button' id = 'cancelar'>cancelar</button></form>");
+
+          $("#musicaList").on("click", "#cancelar", function () {
+              $(self).attr("disabled",false);
+              $("form").remove();
+          });
+
+          $("li form").on("submit", function () {
+              var artista = $.trim($(".artistaClase").val());
+              if (artista == "") {
+                  alert("El artista no puede ser vacío");
+                  return false;
+              }
+              //Validación del título
+              var titulo = $.trim($(".tituloClase").val());
+              if (titulo == "") {
+                  alert("El título no puede ser vacío");
+                  return false;
+              }
+              //Validación de la url
+              var url = $.trim($(".urlClase").val());
+              var pattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/ig;
+              if (url == "" || false == pattern.test(url)) {
+                  alert("La url del archivo no es valido");
+                  return false;
+              }
+              $.ajax({
+                  url: "/api/musica/" + id,
+                  data: JSON.stringify({
+                      artista: artista,
+                      title: titulo,
+                      url: url,
+                  }),
+                  contentType: 'application/json',
+                  method: "put",
+                  success: function () {
+                      alert("Editado con exito");
+                      $("form").remove();
+                      cargarCancion();
+                      return false;
+                  },
+                  error: function () {
+                      alert("Se ha producido un error");
+                      return false;
+                  }
+
+              });
+              return false;
+          });
+      });
+  });
