ad2b08b65915db07f76286a78fd7958d43da761f
Jesus Velasco <jesus.velasco@babel.es>
2016-03-01 11:33:39 +0100

    Formulario añadido al pulsar el botón editar. solucionado problemas al cargar canciones, añadido script a index.html

--- js/cargaCanciones.js
+++ js/cargaCanciones.js
@@ -4,27 +4,21 @@ function cargarCancion() {
         url: "/api/musica/",
         success: function(data) {
             console.log("musica recuperada", data)
-            var html = "";
-            var longitud = data.lenght || 0;
-            console.log(data.lenght);
-            if (longitud == 0) {
-                html = "<li>No hay canciones disponibles</li>";
-            } else {
-                for (var i in data) {
-                    var id = data[i].id;
-                    var artista = data[i].artista;
-                    var title = data[i].title;
-                    var url = data[i].url;
-                    html += "<li>";
-                    html += artista + " ";
-                    html += title + " ";
-                    if (url != "")
-                        html += " (" + url + ")";
-                    html += '<button class="Button Reproducir" id="buttonReproducir" data-musicaid =" ' + id + '">Reproducir</button>';
-                    html += '<button class="Button Editar" id="buttonEditar" data-musicaid ="' + id + '">Editar</button>';
-                    html += '<button class="Button Eliminar" id="buttonEliminar" data-musicaid ="' + id + '">Eliminar</button>';
-                    html += "</li>";
-                }
+            var html = "<li>No hay canciones disponibles</li>";
+            for (var i in data) {
+                var id = data[i].id;
+                var artista = data[i].artista;
+                var title = data[i].title;
+                var url = data[i].url;
+                html = "<li>";
+                html += artista + " ";
+                html += title + " ";
+                if (url != "")
+                    html += " (" + url + ")";
+                html += '<button class="Button Reproducir" id="buttonReproducir" data-musicaid =" ' + id + '">Reproducir</button>';
+                html += '<button class="Button Editar" id="buttonEditar" data-musicaid ="' + id + '">Editar</button>';
+                html += '<button class="Button Eliminar" id="buttonEliminar" data-musicaid ="' + id + '">Eliminar</button>';
+                html += "</li>";
             }
             $("#musicaList").html(html); //innerHTML = html
         }
--- js/editarCancion.js
+++ js/editarCancion.js
@@ -1,21 +1,14 @@
-// $(document).ready(function(){
-//         //Cuando se haga click en el boton eliminar de la información
-//         //de la canción incrustada en nuestro código html se desincrustara
-//         //y se eliminara del servidor.
-//         $("#musicaList").on("click", "#buttonEditar", function () {
-//         console.log("Editar canción");
-//         // var id = $(this).data("musicaid");
-//         // var self = this; //Se refiere al button
-//         // $.ajax({
-//         //     url: "/api/musica/" + id,
-//         //     method: "get",
-//         //     success: function () {
-//                 // var html = "<form novalidate> <input type= 'text' id= 'artista' placeholder= 'Artista' name='artista' required> <input type= 'text' id=titulo placeholder= 'Titulo' name='titulo' required> <input type='text' id='url' placeholder='URL' name='titulo' required> <button type='submit'>Guardar</button><button id = 'salir'>Salir</button></form>";
-//                 // $(self).parent().html(html);
-
-//         //$(".main.content").html(html);
-//         //$("#musicaList li").remove();
-//             });
-//         // });
-//     // });
-// });
+ $(document).ready(function() {
+     //Cuando se haga click en el boton eliminar de la información
+     //de la canción incrustada en nuestro código html se desincrustara
+     //y se eliminara del servidor.
+     $("#musicaList").on("click", "#buttonEditar", function() {
+         console.log("Editar canción");
+         var id = $(this).data("musicaid");
+         var self = this; //Se refiere al button
+         $(self).parent().append("<form novalidate> <input type= 'text' id= 'artista' placeholder= 'Artista' name='artista' required> <input type= 'text' id=titulo placeholder= 'Titulo' name='titulo' required> <input type='text' id='url' placeholder='URL' name='titulo' required> <button type='submit'>Guardar</button><button id = 'salir'>Salir</button></form>");
+         // $.ajax({
+         //     url: "/api/musica/" + id,
+         //     method: "get",
+     });
+ });
