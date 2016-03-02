90373b3585826e8298fad3cbd3864e3a1945ec53
Lautaro German <lautaro.zuniga@babel.es>
2016-03-02 10:17:39 +0100

    CargaCanciones.js: Se ha añadido a un row los datos de la canción.

--- js/cargaCanciones.js
+++ js/cargaCanciones.js
@@ -6,20 +6,27 @@ function cargarCancion() {
             console.log("musica recuperada", data)
             var html ="";
             if (data.length==0)
-            html = "<li>No hay canciones disponibles</li>";
+            html = "<li class ='listaNoData'>No hay canciones disponibles</li>";
             for (var i in data) {
                 var id = data[i].id;
                 var artista = data[i].artista;
                 var title = data[i].title;
                 var url = data[i].url;
                 html += "<li>";
-                html += artista + " ";
-                html += title + " ";
-                if (url != "")
-                    html += " (" + url + ")";
-                html += '<button class="Button Reproducir" id="buttonReproducir" data-musicaid =" ' + id + '">Reproducir</button>';
-                html += '<button class="Button Editar" id="buttonEditar" data-musicaid ="' + id + '">Editar</button>';
-                html += '<button class="Button Eliminar" id="buttonEliminar" data-musicaid ="' + id + '">Eliminar</button>';
+                html +="<div class = 'row'>"; //abre row
+                html += "<div class = 'col-3'>";
+                html += artista + "</div>";
+                html += "<div class = 'col-3'>";
+                html += title + "</div>";
+                html += "<div class = 'col-3'>";
+                html += "(" + url + ")"+"</div>";
+                html += "<div class = 'col-1'>";
+                html += '<button class="Button Reproducir" id="buttonReproducir" data-musicaid =" ' + id + '">Reproducir</button></div>';
+                html += "<div class = 'col-1'>";
+                html += '<button class="Button Editar" id="buttonEditar" data-musicaid ="' + id + '">Editar</button></div>';
+                html += "<div class = 'col-1'>";
+                html += '<button class="Button Eliminar" id="buttonEliminar" data-musicaid ="' + id + '">Eliminar</button</div>';
+                html += "</div>"; //cierra row
                 html += "</li>";
             }
             $("#musicaList").html(html); //innerHTML = html
