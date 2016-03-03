54aef0e1c21558e30e87f4290f7f9888193ca386
Lautaro Germán <laut.german@gmail.com>
2016-03-03 21:33:57 +0100

    intento de responsive..

--- js/agregaCancion.js
+++ js/agregaCancion.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             $(this).html("<i class='fa fa-times-circle-o'></i>");
             html += "<div class = 'row'>";
             html +="<div class ='col-12'>";
-            html += "<form class='formPrincipal' novalidate> <input type= 'text' class = 'artistaClase' id= 'artista' placeholder= 'Artista' name='artista' required> <input type= 'text' class = 'tituloClase' id=titulo placeholder= 'Titulo' name='titulo' required> <input type='text' class = 'urlClase' id='url' placeholder='URL' name='titulo' required> <button type='submit'><i class='fa fa-floppy-o'></i></button></form></div>";
+            html += "<form class='formPrincipal' novalidate> <input type= 'text' class = 'artistaClase' id= 'artista' placeholder= 'Artista' name='artista' required> <input type= 'text' class = 'tituloClase' id=titulo placeholder= 'Titulo' name='titulo' required> <input type='text' class = 'urlClase' id='url' placeholder='URL' name='titulo' required> <button type='submit' class='save'><i class='fa fa-floppy-o'></i></button></form></div>";
             html+="</div>";
             $(".main.content").html(html);
 			$(this).data("iconoid","x");
